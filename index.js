1769c3595007d5f36861fc8ab731cb80d7b168c2
Golden1636 <okeamagolden@yahoo.com>
2026-05-08 19:46:28 +0100

    rendered the checkout section with the selected foods from the menu

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 import { menuArray } from "./data.js";
 
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.add) {
+    handleAddButtonClick(e.target.dataset.add);
+  }
+});
+
+let foodsOrdered = [];
+
+function handleAddButtonClick(menuId) {
+  const targetMenuObj = menuArray.filter(function (menu) {
+    return menu.id === Number(menuId);
+  })[0];
+
+  foodsOrdered.push(targetMenuObj);
+  renderFoodsOrdered();
+}
+
+function renderFoodsOrdered() {
+  document.getElementById("orders").innerHTML = getOrderedFoodsHtml();
+
+  document.getElementById("checkout-section").style.display = "block";
+}
+
+function getOrderedFoodsHtml() {
+  return foodsOrdered
+    .map((food) => {
+      return `
+      <li class="order">
+        <span class="order-name">${food.name}</span>
+        <button class="btn btn-remove">remove</button>
+        <span class="order-price">$${food.price}</span>
+      </li>
+    `;
+    })
+    .join("");
+}
+
 function getMenuFeed() {
   let menuListItems = "";
 
@@ -14,7 +51,8 @@ function getMenuFeed() {
           <p class="ingredients">${menu.ingredients.join(", ")}</p>
           <p class="price">$${menu.price}</p>
         </div>
-        <buttton class="btn add-btn"><p>+</p></buttton>
+
+        <buttton class="btn add-btn" data-add="${menu.id}">+</buttton>
       </li>
     `;
   });
@@ -26,3 +64,5 @@ function render() {
   document.getElementById("menu-feed").innerHTML = getMenuFeed();
 }
 render();
+
+// Add focus states to the hover states in the CSS file.
